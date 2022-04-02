112669b3aa429214d5ebdd56f72debfd279eb4d1
Sumanth Tellakula <56023590+sumanthrt@users.noreply.github.com>
2022-04-02 18:43:29 +0530

    Checkpoint 2 - header and logo added with animation

---
+++ src/common/header/Header.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import './Header.css';
+import logo from './logo.svg';
+
+class Header extends Component {
+    render() {
+        return(
+            <div className='header'>
+                <img src={logo} id='logo'/>
+            </div>
+        )
+    }
+}
+
+export default Header;
